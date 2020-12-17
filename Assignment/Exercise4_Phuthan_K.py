d8d77523e206d0c9ae8c4adf7bc6052e5365e8e5
lnwyong <73463787+lnwyong@users.noreply.github.com>
2020-12-17 15:35:23 +0700

    Create Exercise4_Phuthan_K.py

---
+++ Assignment/Exercise4_Phuthan_K.py
@@ -0,0 +1,10 @@
+feScore = 50
+gbScore = 70
+icsScore = 80
+cpScore = 76
+
+print("------------ Your Score ------------")
+print("Foundation English               : ",feScore)
+print("General Business                 : ",gbScore)
+print("Introduction to Computer Systems : ",icsScore)
+print("Computer Programming             : ",cpScore)
